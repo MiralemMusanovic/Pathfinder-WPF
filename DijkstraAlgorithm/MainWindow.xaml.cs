00bd4254a2d8b78e57c665054c693247be498095
Miralem Mušanović <44951439+MiralemMusanovic@users.noreply.github.com>
2020-06-08 23:42:43 +0200

    UI kinda done

--- DijkstraAlgorithm/MainWindow.xaml.cs
+++ DijkstraAlgorithm/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -25,8 +26,54 @@ namespace DijkstraAlgorithm
             InitializeComponent();
         }
 
-        private void Node00_Click(object sender, RoutedEventArgs e)
+        public bool isStartPoint = false;
+        public bool isEndPoint = false;
+        public bool isWalls = false;
+        public string coords;
+
+        private void ChangeColor(object sender, RoutedEventArgs e)
+        {   
+            if (isStartPoint == true)
+            {
+                ((Button)sender).Background = Brushes.Orange;
+                coords = (string)((Button)sender).Content;
+                isStartPoint = false;
+            }
+            else if (isEndPoint == true)
+            {
+                ((Button)sender).Background = Brushes.DodgerBlue;
+                isEndPoint = false;
+            }
+            else if (isWalls == true)
+            {
+                ((Button)sender).Background = Brushes.DarkGray;
+            }
+        }
+
+        private void StartPoint_Click(object sender, RoutedEventArgs e)
         {
+            isStartPoint = true;
+            isEndPoint = false;
+            isWalls = false;
+    }
+
+        private void EndPoint_Click(object sender, RoutedEventArgs e)
+        {
+            isStartPoint = false;
+            isEndPoint = true;
+            isWalls = false;
+        }
+
+        private void Walls_Click(object sender, RoutedEventArgs e)
+        {
+            isStartPoint = false;
+            isEndPoint = false;
+            isWalls = true;
+        }
+
+        private void StartDijkstra_Click(object sender, RoutedEventArgs e)
+        {
+            
         }
     }
 }
