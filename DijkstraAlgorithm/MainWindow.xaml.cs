d46ac8b4184e4df94a088a52e8efb172c2826d2a
Miralem Mušanović <44951439+MiralemMusanovic@users.noreply.github.com>
2020-06-10 15:06:23 +0200

    Update MainWindow.xaml.cs

--- DijkstraAlgorithm/MainWindow.xaml.cs
+++ DijkstraAlgorithm/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -21,81 +22,96 @@ namespace DijkstraAlgorithm
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        public bool startPointBtn = false;
+        public bool endPointBtn = false;
+        public bool wallsBtn = false;
+        public int starterRow;
+        public int starterCol;
+        public int finishRow;
+        public int finishCol;
         public MainWindow()
         {
             InitializeComponent();
+            makeGrid();
+        }
+
+        private void makeGrid()
+        {
             gamePanel.Rows = 10;
             gamePanel.Columns = 20;
             for (int i = 0; i < gamePanel.Rows; i++)
             {
-                for(int j = 0; j < gamePanel.Columns; j++)
+                for (int j = 0; j < gamePanel.Columns; j++)
                 {
                     Button b = new Button();
                     Thickness margin = b.Margin;
                     margin.Left = 2;
                     margin.Bottom = 2;
                     b.Margin = margin;
-                    b.Content = $"{i} {j}";
                     b.Width = 50;
                     b.Height = 50;
                     b.Click += ChangeColor;
                     gamePanel.Children.Add(b);
                 }
-
             }
         }
 
-        public bool isStartPoint = false;
-        public bool isEndPoint = false;
-        public bool isWalls = false;
-        public string starterCoords;
-        public string endCoords;
-
         private void ChangeColor(object sender, RoutedEventArgs e)
         {   
-            if (isStartPoint == true)
+            if (startPointBtn == true)
             {
-                ((Button)sender).Background = Brushes.Orange;
-                starterCoords = (string)((Button)sender).Content;
-                isStartPoint = false;
+                
+                    Button clicked = (Button)sender;
+                    clicked.Background = Brushes.Orange;
+                    starterRow = (int)clicked.GetValue(Grid.RowProperty);
+                    starterCol = (int)clicked.GetValue(Grid.ColumnProperty);
+                    startPointBtn = false;
             }
-            else if (isEndPoint == true)
+            else if (endPointBtn == true)
             {
-                ((Button)sender).Background = Brushes.DodgerBlue;
-                endCoords = (string)((Button)sender).Content;
-                isEndPoint = false;
+                
+                Button clicked = (Button)sender;
+                clicked.Background = Brushes.DodgerBlue;
+                finishRow = (int)clicked.GetValue(Grid.RowProperty);
+                finishCol = (int)clicked.GetValue(Grid.ColumnProperty);
+                endPointBtn = false;
             }
-            else if (isWalls == true)
+            else if (wallsBtn == true)
             {
-                ((Button)sender).Background = Brushes.DarkGray;
+                Button clicked = (Button)sender;
+                clicked.Background = Brushes.DarkGray;
             }
         }
 
         private void StartPoint_Click(object sender, RoutedEventArgs e)
         {
-            isStartPoint = true;
-            isEndPoint = false;
-            isWalls = false;
+            startPointBtn = true;
+            endPointBtn = false;
+            wallsBtn = false;
     }
 
         private void EndPoint_Click(object sender, RoutedEventArgs e)
         {
-            isStartPoint = false;
-            isEndPoint = true;
-            isWalls = false;
+            startPointBtn = false;
+            endPointBtn = true;
+            wallsBtn = false;
         }
 
         private void Walls_Click(object sender, RoutedEventArgs e)
         {
-            isStartPoint = false;
-            isEndPoint = false;
-            isWalls = true;
+            startPointBtn = false;
+            endPointBtn = false;
+            wallsBtn = true;
         }
 
         private void StartDijkstra_Click(object sender, RoutedEventArgs e)
         {
-            
+            startPointBtn = false;
+            endPointBtn = false;
+            wallsBtn = false;
+
+
+            gamePanel.Children.Cast<Button>().First(e => Grid.GetRow(e) == 1 && Grid.GetColumn(e) == 1).Background = Brushes.Black;
         }
     }
 }
